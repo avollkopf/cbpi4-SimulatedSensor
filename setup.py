bd9400f3adda5cc4e01a33dd959789c89bd7fae6
prash3r <github@prash3r.de>
2022-09-16 16:37:49 +0200

    incrementing pypi version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='cbpi4_SimulatedSensor',
-      version='0.0.1',
+setup(name='cbpi4-SimulatedSensor',
+      version='0.0.2',
       description='This plugin allowes you to simulate a sensor. The sensor will check if a Kettle is currently heating and will increase its value by a configurable amount. If the Kettle is not heating the temperature will be decreased by a configurable amount.',
       author='prash3r',
       author_email='pypi@prash3r.de',
-      url='https://github.com/prash3r/cbpi_SimulatedSensor',
+      url='https://pypi.org/project/cbpi4-SimulatedSensor/',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
