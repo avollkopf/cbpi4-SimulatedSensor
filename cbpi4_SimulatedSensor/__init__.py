113535f4da175de3c76777ba027861a9c1c08dd9
prash3r <github@prash3r.de>
2022-09-16 16:13:45 +0200

    changed to used Actor instead of kettle. resolves #1 and adds a notification on hardware config change to warn about production usage.

--- cbpi4_SimulatedSensor/__init__.py
+++ cbpi4_SimulatedSensor/__init__.py
@@ -1,5 +1,6 @@
 
 # -*- coding: utf-8 -*-
+from email import message
 import os
 import datetime
 from aiohttp import web
@@ -8,53 +9,50 @@ from unittest.mock import MagicMock, patch
 import asyncio
 import random
 from cbpi.api import *
+from cbpi.api.dataclasses import NotificationType
 
-logger = logging.getLogger(__name__)
-
-@parameters([Property.Number(label="HeatingRate", description="Simulated: maximal heating rate when heating for a long time"), 
-             Property.Number(label="CoolingRate", description="Simulated: maximal cooling rate when heating is off for a long time"),
-             Property.Kettle(label="SimulatedKettle", description="the kettle which temperature should be simulated"),
+@parameters([Property.Number(label="HeatingRate", default_value=0.1, description="Simulated: heating rate per second (decimal seperator is a dot and float can be negative to simulate a cooling actor)"), 
+             Property.Number(label="CoolingRate", default_value=0.01, description="Simulated: cooling rate per second (decimal seperator is a dot and float can be negative to simulate a cooling actor)"),
+             Property.Actor(label="HeatingActor", description="the actor that will result in the simulated temperature to rise (or fall)."),
              Property.Select(label="LogSimulatedSensor",options=["Yes","No"], description="on the setting (Yes) the simulated sensor will be logged as well. On the setting (No) there wont be any logging for this simulated sensor.")])
 class SimulatedSensor(CBPiSensor):
     
     def __init__(self, cbpi, id, props):
         super(SimulatedSensor, self).__init__(cbpi, id, props)
-        self.value = 0
+        self.value = 0.0
         self.running = True
-        logger.info("Kettle ID:" + str(self.props))
+        self.logger = logging.getLogger(__name__)
+        self.actor = self.cbpi.actor.find_by_id(self.props.HeatingActor)
+        self.cbpi.notify(title="DEVELOPMENT ONLY", message="The cbpi4-SimulatedSensor plugin should NOT be used in production!", type=NotificationType.WARNING)
+        self.logger.warning("the plugin cbpi4-SimulatedSensor should not be installed in a production environment and should only be used in the dev container")
 
     async def run(self):
-        HeaterID = self.props.SimulatedHeater
         while self.running == True:
-        #while True:
-            HeaterID = self.cbpi.kettle.find_by_id(self.props.SimulatedKettle).heater
-            Heater = self.cbpi.actor.find_by_id(HeaterID)
-            HeaterState = self.cbpi.actor.find_by_id(HeaterID).instance.state
+            HeaterState = self.cbpi.actor.find_by_id(self.props.HeatingActor).instance.state
             potentialNewValue = self.value
             if HeaterState :
-                potentialNewValue = round(self.value + float(self.props.HeatingRate), 2)
+                potentialNewValue = round(float(self.value) + float(self.props.HeatingRate), 2)
             else:
-                potentialNewValue = round(self.value - float(self.props.CoolingRate), 2)
+                potentialNewValue = round(float(self.value) - float(self.props.CoolingRate), 2)
             clampedValue = clamp(potentialNewValue,-20,120)
             if clampedValue != self.value :
                 self.value = clampedValue
-                #print(self.value)
                 self.push_update(self.value)
-            if self.props.get("LogSimulatedSensor", "Yes") == "Yes":
-                self.log_data(self.value)
+                if self.props.get("LogSimulatedSensor", "Yes") == "Yes":
+                    self.log_data(self.value)
             await asyncio.sleep(1)
     
     def get_state(self):
         return dict(value=self.value)
-
-def setup(cbpi):
-    cbpi.plugin.register("SimulatedSensor", SimulatedSensor)
-    pass
-
-def clamp(n, minn, maxn):
+    
+def clamp(n : float, minn : float, maxn : float):
     if n < minn:
         return minn
     elif n > maxn:
         return maxn
     else:
         return n
+
+def setup(cbpi):
+    cbpi.plugin.register("SimulatedSensor", SimulatedSensor)
+    pass
